d2f649cd89ff3eba9acead21f93e646c7087184b
selfup <boudinot.regis@yahoo.com>
2018-10-13 12:47:20 -0500

    socket route

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -6,7 +6,7 @@ const SUB = 'game_sub';
 export default class Handler {
   constructor(channel = 'room:game') {
     const smache = process.env.NODE_ENV === 'production'
-      ? 'wss://thawing-reef-15819.herokuapp.com/'
+      ? 'wss://thawing-reef-15819.herokuapp.com/socket'
       : 'ws://localhost:4000/socket';
     
     const socket = new Socket(smache, {});
